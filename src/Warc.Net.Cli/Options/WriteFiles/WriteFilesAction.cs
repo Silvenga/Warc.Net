fdf05018e78f0c277b1237f6201619eb9b193aba
Mark Lopez <m@silvenga.com>
2021-03-10 21:30:46 -0600

    Improved file writing.

--- src/Warc.Net.Cli/Options/WriteFiles/WriteFilesAction.cs
+++ src/Warc.Net.Cli/Options/WriteFiles/WriteFilesAction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using Warc.Net.Parsing;
@@ -41,23 +42,50 @@ namespace Warc.Net.Cli.Options.WriteFiles
                     var domain = new Uri(targetUri).Host;
 
                     var targetFilePath = new Uri(targetUri).AbsolutePath;
+
+                    // /post/ -> /post/index.html
+                    // / -> /index.html
+                    if (targetFilePath.EndsWith("/"))
+                    {
+                        targetFilePath = targetFilePath + "index.html";
+                    }
+
+                    // /index.html -> index.html
                     if (targetFilePath.StartsWith("/"))
                     {
                         targetFilePath = targetFilePath.Substring(1);
                     }
 
+                    // // /index.html -> example.com/index.html
                     targetFilePath = Path.GetFullPath(Path.Join(outputFilePrefix, domain, targetFilePath));
 
-                    var directory = new FileInfo(targetFilePath).DirectoryName;
-                    if (!Directory.Exists(directory))
+                    if (!targetFilePath.StartsWith(outputFilePrefix))
                     {
-                        Directory.CreateDirectory(directory!);
+                        throw new Exception($"Possible dangerous path transverse detected for '{targetUri}'.");
                     }
 
                     await _writer.Info($"Writing file '{targetFilePath}' from '{targetUri}'.");
 
-                    var file = File.OpenWrite(targetFilePath);
-                    await file.WriteAsync(warcRecord.Payload.Data);
+                    if (File.Exists(targetFilePath))
+                    {
+                        await _writer.Warn($"File '{targetFilePath}' already exists and will be overwritten.");
+                    }
+
+                    try
+                    {
+                        var directory = new FileInfo(targetFilePath).DirectoryName;
+                        if (!Directory.Exists(directory))
+                        {
+                            Directory.CreateDirectory(directory!);
+                        }
+
+                        await using var file = File.OpenWrite(targetFilePath);
+                        await file.WriteAsync(warcRecord.Payload.Data);
+                    }
+                    catch (Exception e)
+                    {
+                        await _writer.Warn($"An error occurred while writing '{targetFilePath}', moving to next. {e}");
+                    }
                 }
             }
 
@@ -76,7 +104,16 @@ namespace Warc.Net.Cli.Options.WriteFiles
                 else
                 {
                     await using var fileStream = File.OpenRead(input);
-                    await reader.WriteAsync(fileStream);
+
+                    if (input.EndsWith(".gz"))
+                    {
+                        await using var compressionStream = new GZipStream(fileStream, CompressionMode.Decompress);
+                        await reader.WriteAsync(compressionStream);
+                    }
+                    else
+                    {
+                        await reader.WriteAsync(fileStream);
+                    }
 
                     await _writer.Info($"Input file '{input}' has been completely buffered.");
                 }
